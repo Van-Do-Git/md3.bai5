091d6d44f41367f9ec8e233b090210a89e37513d
unknown <mittervan@gmail.com>
2021-08-18 09:25:44 +0700

    thuc hanh stored procedue

--- src/practice_index.sql
+++ src/practice_index.sql
@@ -6,3 +6,4 @@ EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.';
 EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
 ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
 ALTER TABLE customers DROP INDEX idx_full_name;
+
---
+++ src/practiec_stored.sql
@@ -0,0 +1,32 @@
+use classicmodels;
+DELIMITER //
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+    SELECT * FROM customers;
+
+END //
+
+DELIMITER ;
+
+call findAllCustomers();
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+    SELECT * FROM customers where customerNumber = 175;
+
+END //
+
+call findAllCustomers();
+
+drop procedure findAllCustomers;
+
+call findAllCustomers();
+
